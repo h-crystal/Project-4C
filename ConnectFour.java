e63f4bf0e7287f15de744231f94b16bd97a5ce2f
h-crystal <harriscrystal004@gmail.com>
2026-05-29 19:35:04 +0000

    honestly kinda lost,

--- ConnectFour.java
+++ ConnectFour.java
@@ -16,36 +16,55 @@ public class ConnectFour {
         String [][] grid= new String[6][7]; //six rows of spaces
         //grid[6][7]={" "," "," "," "," "," "};
 
-        boolean gamePlaying = true;
+        boolean gamePlaying= true;
         int turn=0;
         String redDisk=null;
         String yellowDisk=null;
-
+        int pieceCol;
 
         System.out.println("Welcome to Connect Four! Let's Start!");
         while (gamePlaying==true){
             
             if(turn%2==0){
                 System.out.println("Player 1, drop your red disk at column (0-6): ");
-                redDisk= input.nextLine();
+                pieceCol= input.nextInt();
 
             }
 
             else{
                 System.out.println("Player 2, drop your yellow disk at column (0-6): ");
-                yellowDisk= input.nextLine();
+                pieceCol= input.nextInt();
 
             }
 
             displayGrid(grid);
             checkHorizontal(grid);
-            checkVertical(grid);
-
-            /*
-            checkMajor(grid);
+    /*
             checkMinor(grid);
             */
-            turn++;
+           if (checkHorizontal(grid)){ 
+                checkHorizontal(grid);
+                gamePlaying = false;
+            }
+
+            else if (checkVertical(grid)){ 
+                checkVertical(grid);
+                gamePlaying = false;
+            }
+
+            else if (checkMajor(grid)){
+                checkMajor(grid); 
+                gamePlaying = false;
+            }
+
+            else if (checkMinor(grid)){ 
+                checkMinor(grid);
+                gamePlaying = false;
+            }
+
+            else{
+                turn++;
+            }
         }
 
     }    
@@ -61,11 +80,11 @@ public class ConnectFour {
                 System.out.print(grid[row][col]+"|");
 
                 if(turn%2==0){
-                    System.out.print(redDisk);
+                    System.out.print("R");
                 }
 
                 else{
-                    System.out.print(yellowDisk);
+                    System.out.print("Y");
                 }
                 
             }
@@ -76,93 +95,68 @@ public class ConnectFour {
     public boolean checkHorizontal(String[][] grid){
 
         boolean win=false;
-        int score=0;
-        String check= grid[row][col];
+        int scoreR=0;
+        int scoreY=0;
 
-        if(turn%2==0){
+        for(int row=0;row<grid.length; row++){
 
             for(int col= 0; col<grid[0].length ;col++){
+                String check= grid[row][col];
+                if(check.equals("R")){
+                    scoreR++;
+                }      
 
-                for(int row=0;row<grid.length; row++){
-                    String check= grid[row][col];
-                    if(check.equals("R")){
-                        score= score+1;
-                    }      
-                }
-            }
-
-            if(score==4){
-                win= true;
-                gamePlaying=false;
-                return win;
+                else if(check.equals("Y")){
+                    scoreY++;
+                } 
             }
         }
 
-        else{
-
-            for(int col= 0; col<grid[0].length;col++){
-
-                for(int row=0;row<grid.length; row++){
-                    String check= grid[row][col];
-                    if(check.equals("Y")){
-                        score= score+1;
-                    }      
-                }
-            }
-
-            if(score==4){
-                win= true;
-                gamePlaying=false;
-                return win;
-            }
+        if(scoreR==4){
+            System.out.println("Player 1 won");
+            win= true;
         }
 
+        else if(scoreY==4){
+            System.out.println("Player 2 won");
+            win= true;
+        }
+
+        return win;
     }
+
+
 
     public boolean checkVertical(String[][] grid){
         boolean win=false;
-        int score=0;
-        
+        int scoreR=0;
+        int scoreY=0;
 
-        if(turn%2==0){
+        for(int col= 0; col<grid[0].length ;col++){
 
-            for(int row= 0; row<grid.length ;row++){
+            for(int row=0;row<grid.length; row++){
+                String check= grid[row][col];
+                if(check.equals("R")){
+                    scoreR++;
+                }      
 
-                for(int col=0;col<grid[0].length; col++){
-                    String check= grid[row][col];
-                    if(check.equals("R")){
-                        score= score+1;
-                    }      
-                }
-            }
-
-            if(score==4){
-                win= true;
-                gamePlaying=false;
-                return win;
+                else if(check.equals("Y")){
+                    scoreY++;
+                } 
             }
         }
 
-        else{
-
-            for(int row= 0; row<grid.length ;row++){
-
-                for(int col=0;col<grid[0].length; col++){
-                    String check= grid[row][col];
-                    if(check.equals("Y")){
-                        score= score+1;
-                    }      
-                }
-            }
-
-            if(score==4){
-                win= true;
-                gamePlaying=false;
-                return win;
-            }
+        if(scoreR==4){
+            System.out.println("Player 1 won");
+            win= true;
         }
 
+        else if(scoreY==4){
+            System.out.println("Player 2 won");
+            win= true;
+        }
 
+        return win;
     }
 
 /*
